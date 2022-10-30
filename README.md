e29dc2fb23046804ff9fdb86435aa47e67ae7e06
Kludron <kludron@lukewarmsecurityinfo.com>
2022-10-31 10:53:24 +1100

    Updated?

---
+++ README.md
@@ -0,0 +1,14 @@
+
+## What is this?
+
+This is basically a quick script to help with CTF's to enumerate all open ports.
+
+*NOTE: This does not do an in-depth scan, it simply grabs the open ports
+of the server/s associated with the target that you provide.*
+
+For example, if you type in 
+
+## Quickstart
+
+There are a few required programs that need to be
+
--- enum.sh
+++ enum.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
 printhelp() {
-echo "usage: $0 [options] <-t|--target> <target>
-	Options				Description
-	-h | --help			Shows this prompt
-	-t | --target <target>		Selects a target to scan (hostname or ip address permitted)
-	-o | --output <filename>	Sends the enumerated ports to the target output file. Default to stdout
-	-p | --ports <range>		Specifies the ports to check. Default: 1-65535
-	-s | --scanner <scannername>	Specifies the scanner program to use. Options: nmap, masscan
+echo "
+usage: $0 [options] <-t|--target> <target>
+Options                         Description
+-h | --help                     Shows this prompt
+-t | --target <target>	        Selects a target to scan (hostname or ip address permitted)
+-o | --output <filename>        Sends the enumerated ports to the target output file. Default to stdout
+-p | --ports <range>            Specifies the ports to check. Default: 1-65535
+-s | --scanner <scannername>	Specifies the scanner program to use. Options: nmap, masscan
 
-	Examples							Description
-	
-	sh $0 -t google.com -p 1-1000 -s masscan	This scans the ip address that 'google.com' resolves to
-									over ports 1,2,3,4,..,1000 using masscan, printing output
-									to stdout.
+Examples                        	            Description
+sh $0 -t google.com -p 1-1000 -s masscan        This scans the ip address that 'google.com' resolves to
+                                                over ports 1,2,3,4,..,1000 using masscan, printing output
+                                                to stdout.
 
-	sh $0 -t yahoo.com				This scans the ip address that 'yahoo.com' resolves to
-									over all ports (1-65535) using nmap, printing output to 
-									stdout.
+sh $0 -t yahoo.com                              This scans the ip address that 'yahoo.com' resolves to
+                                                over all ports (1-65535) using nmap, printing output to 
+                                                stdout.
 
-	sh $0 -t 192.168.0.1 -o ports.txt		This scans 192.168.0.1 over all ports using nmap and 
-									saves the output to the file 'ports.txt'
-		
+sh $0 -t 192.168.0.1 -o ports.txt               This scans 192.168.0.1 over all ports using nmap and 
+                                                saves the output to the file 'ports.txt'
 "
 }
-
 while [[ "$#" -gt 0 ]]; do
 	case $1 in
 		-h|--help) 
@@ -46,7 +44,7 @@ while [[ "$#" -gt 0 ]]; do
 			shift 
 			shift
 			;;
-		-s|--scanner)
+        -s|--scanner)
 			scanner="$2";
 			if [ -z "$scanner" ]; then
 				>&2 echo "Scanner option found, but no scanner specified"
@@ -90,10 +88,10 @@ else
 
 	# Check which scanner to use
 	if [ "$scanner" == "nmap" ]; then
-		scan=$(sudo nmap $target -p$ports --open -T4)
+        scan=$(sudo nmap $target -p$ports --open -T4)
 		result=$(grep ^[0-9] <<< $scan | awk -F "/" '{print $1}' | tr '\n' ',' | awk '{print substr($0, 1, length($0)-1)}')
 	elif [ "$scanner" == "masscan" ]; then
-		scan=$(sudo masscan $target -p$ports --open-only --rate 100)
+        scan=$(sudo masscan $target -p$ports --open-only --rate 100)
 		result=$(awk '{print $4}' <<< $scan | awk -F "/" '{print $1}' | tr '\n' ',' | awk '{print substr($0, 1, length($0)-1)}')
 	fi
 
